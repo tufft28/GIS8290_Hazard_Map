8efdfce7408f1c288b08f2a1e7bf87e5e43280d7
Maureen McFarlane <m.koenig.mcfarlane@gmail.com>
2015-11-23 23:55:37 +0000

    combined popup and toggle code
    Teran and Maureen combine code

--- script.js
+++ script.js
@@ -3,16 +3,32 @@
 require([
   "esri/map",
   "esri/layers/ArcGISDynamicMapServiceLayer",
+  "esri/layers/FeatureLayer",
   "esri/layers/ImageParameters",
+  "esri/dijit/Popup",
+  "esri/dijit/PopupTemplate",
   "esri/dijit/InfoWindow",
   "esri/InfoTemplate",
+  "esri/symbols/SimpleFillSymbol",
+  "esri/Color",
+  "dojo/dom-class",
   "dojo/dom",
   "dojo/dom-construct",
   "dojo/on",
+  "dojox/charting/Chart",
+  "dojox/charting/themes/Dollar",
   "dojo/query",
   "dojo/domReady!"
 ],
-  function (Map, ArcGISDynamicMapServiceLayer, ImageParameters, InfoWindow, InfoTemplate, dom, domConstruct, on, query) {
+  function (Map, ArcGISDynamicMapServiceLayer, FeatureLayer, ImageParameters, Popup, PopupTemplate, InfoWindow, InfoTemplate, SimpleFillSymbol, Color, domClass, dom, domConstruct, on, Chart, theme, query) {
+
+  var fill = new SimpleFillSymbol("solid", null, new Color("#A4CE67"));
+  var popup = new Popup({
+      fillSymbol: fill,
+      titleInBody: false
+  }, domConstruct.create("div"));
+  domClass.add(popup.domNode, "dark");
+
     var layer, visibleLayerIds = [];
     var infoWindow = new InfoWindow({}, domConstruct.create("div"));
     infoWindow.startup();
@@ -22,16 +38,45 @@ require([
       center: [-93, 44.9],
       zoom: 11,
       basemap: "topo",
-      infoWindow: infoWindow
+      infoWindow: popup
     });
 
-    map.infoWindow.setTitle("Testing");
-    map.infoWindow.setContent("This county is fucked");
+    var template = new PopupTemplate({
+      title: "{NAME}, County",
+      description: "Natural Hazard Risk",
+      fieldInfos: [{ //define field infos so we can specify an alias
+        fieldName: "freq",
+        label: "Wildfire"
+      },{
+        fieldName: "freq",
+        label: "Hurricane"
+      },{
+        fieldName: "freq",
+        label: "Flood"
+      }],
+      mediaInfos:[{ //define the bar chart
+        caption: "",
+        type:"barchart",
+        value:{
+          theme: "Dollar",
+          fields:["freq","freq","freq"]
+        }
+      }]
+    });
 
-    function showInfoWindow(evt){
-      map.infoWindow.show(evt.mapPoint, map.getInfoWindowAnchor(evt.screenPoint));
-    }
-    map.on("click", showInfoWindow)
+    var featureLayer = new FeatureLayer("http://services.arcgis.com/8df8p0NlLFEShl0r/arcgis/rest/services/wildfire_poly/FeatureServer/0",{
+      mode: FeatureLayer.MODE_ONDEMAND,
+      outFields: ["*"],
+      infoTemplate: template
+    });
+    map.addLayer(featureLayer);
+    // map.infoWindow.setTitle("Testing");
+    // map.infoWindow.setContent("This county is fucked");
+    //
+    // function showInfoWindow(evt){
+    //   map.infoWindow.show(evt.mapPoint, map.getInfoWindowAnchor(evt.screenPoint));
+    // }
+    // map.on("click", showInfoWindow)
 
     //Use the ImageParameters to set the visibleLayerIds layers in the map service during ArcGISDynamicMapServiceLayer construction.
     var imageParameters = new ImageParameters();
