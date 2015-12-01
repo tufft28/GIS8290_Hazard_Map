52e961142dee9af5a8e906a93f48d581648892ec
Maureen McFarlane <m.koenig.mcfarlane@gmail.com>
2015-12-01 23:59:39 +0000

    added main html pages
    yep

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ require([
   "dojox/charting/Chart",
   "dojox/charting/themes/Dollar",
   "dojo/query",
+  "dojo/NodeList-traverse",
   "dojo/domReady!"
 ],
   function (Map, ArcGISDynamicMapServiceLayer, FeatureLayer, ImageParameters, Popup, PopupTemplate, InfoWindow, InfoTemplate, SimpleFillSymbol, Color, domClass, dom, domConstruct, on, Chart, theme, query) {
@@ -69,7 +70,7 @@ require([
       outFields: ["*"],
       infoTemplate: template
     });
-    map.addLayer(featureLayer);
+    //map.addLayer(featureLayer);
     // map.infoWindow.setTitle("Testing");
     // map.infoWindow.setContent("This county is fucked");
     //
@@ -84,46 +85,62 @@ require([
     imageParameters.layerOption = ImageParameters.LAYER_OPTION_HIDE;
     //can also be: LAYER_OPTION_EXCLUDE, LAYER_OPTION_HIDE, LAYER_OPTION_INCLUDE
 
-    layer = new ArcGISDynamicMapServiceLayer("http://localhost:6080/arcgis/rest/services/Hazards/HazardsTest/MapServer",
+    layer = new ArcGISDynamicMapServiceLayer("http://localhost:6080/arcgis/rest/services/Hazards/CrisisMappingFinal/MapServer",
         {"imageParameters": imageParameters});
     map.addLayer(layer);
 
-    //dom.byId("layer0CheckBox").addEventListener("click", toggle(this));
-    //dom.byId("layer1CheckBox").addEventListener("click", toggle(this));
-    //on(dom.byId("layer0CheckBox"), "change", toggle(this));
-    //on(dom.byId("layer1CheckBox"), "change", toggle(this));
-    on(dom.byId("layer0CheckBox"), "change", updateLayerVisibility);
-    on(dom.byId("layer1CheckBox"), "change", updateLayerVisibility);
-
-    function toggle (box) {
-      var inputs = query(".list_item");
-      visibleLayerIds = [];
-      if(box.checked) {
-        visibleLayerIds.push(box.value);
-        layer.setVisibleLayers(visibleLayerIds);
-        for (var i = 0; i < inputs.length; i++) {
-          if (inputs[i].value != box.value) {
-            inputs[i].checked === false;
-          }
+    on(dom.byId("layer0CheckBox"), "change", function(){
+      var box = dom.byId("layer0CheckBox");
+      var visible = [];
+      if (box.checked){
+        var not_visible = query(".tornadoes").siblings();
+        for (var i = 0; i < not_visible.length; i++) {
+          not_visible[i].checked = false;
         }
+        visible.push(box.value);
+        layer.setVisibleLayers(visible);
+      }
+      else {
+        visible.push(-1);
+        layer.setVisibleLayers(visible);
       }
     }
-
-    function updateLayerVisibility () {
-      var inputs = query(".list_item");
-      var inputCount = inputs.length;
-      visibleLayerIds = [];
-
-      for (var i = 0; i < inputCount; i++) {
-        if (inputs[i].checked) {
-          visibleLayerIds.push(inputs[i].value);
+  );
+    on(dom.byId("layer1CheckBox"), "change", function(){
+      var box = dom.byId("layer1CheckBox");
+      var visible = [];
+      if (box.checked){
+        var not_visible = query(".hurricanes").siblings();
+        for (var i = 0; i < not_visible.length; i++) {
+          not_visible[i].checked = false;
         }
+        visible.push(box.value);
+        layer.setVisibleLayers(visible);
       }
-
-      if (visibleLayerIds.length === 0) {
-        visibleLayerIds.push(-1);
+      else {
+        visible.push(-1);
+        layer.setVisibleLayers(visible);
       }
-
-      layer.setVisibleLayers(visibleLayerIds);
     }
+  );
+    //on(dom.byId("layer0CheckBox"), "change", updateLayerVisibility);
+    //on(dom.byId("layer1CheckBox"), "change", updateLayerVisibility);
+
+    // function updateLayerVisibility () {
+    //   var inputs = query(".list_item");
+    //   var inputCount = inputs.length;
+    //   visibleLayerIds = [];
+    //
+    //   for (var i = 0; i < inputCount; i++) {
+    //     if (inputs[i].checked) {
+    //       visibleLayerIds.push(inputs[i].value);
+    //     }
+    //   }
+    //
+    //   if (visibleLayerIds.length === 0) {
+    //     visibleLayerIds.push(-1);
+    //   }
+    //
+    //   layer.setVisibleLayers(visibleLayerIds);
+    // }
   });
