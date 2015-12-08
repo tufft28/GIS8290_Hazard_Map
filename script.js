536f6461d570267bbf2e1b4cbbbd2ffe61f62991
Maureen McFarlane <m.koenig.mcfarlane@gmail.com>
2015-12-08 19:24:28 +0000

    cleaned up js functions and map/list layout

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ require([
   "dojox/charting/themes/Dollar",
   "dojo/query",
   "dojo/NodeList-traverse",
+  "dijit/layout/BorderContainer",
+  "dijit/layout/ContentPane",
   "dojo/domReady!"
 ],
   function (Map, ArcGISDynamicMapServiceLayer, FeatureLayer, ImageParameters, Popup, PopupTemplate, InfoWindow, InfoTemplate, SimpleFillSymbol, Color, domClass, dom, domConstruct, on, Chart, theme, query) {
@@ -30,7 +32,6 @@ require([
   }, domConstruct.create("div"));
   domClass.add(popup.domNode, "dark");
 
-    var layer, visibleLayerIds = [];
     var infoWindow = new InfoWindow({}, domConstruct.create("div"));
     infoWindow.startup();
     var infoTemplate = new InfoTemplate("Testing", "${*}");
@@ -65,30 +66,25 @@ require([
       }]
     });
 
+    //feature layer containing all risks and set as transparent. popup pulls from this layer for display information
     var featureLayer = new FeatureLayer("http://services.arcgis.com/8df8p0NlLFEShl0r/arcgis/rest/services/AllRisks_Popup/FeatureServer/0",{
       mode: FeatureLayer.MODE_ONDEMAND,
       outFields: ["*"],
       infoTemplate: template
     });
     map.addLayer(featureLayer);
-    // map.infoWindow.setTitle("Testing");
-    // map.infoWindow.setContent("This county is fucked");
-    //
-    // function showInfoWindow(evt){
-    //   map.infoWindow.show(evt.mapPoint, map.getInfoWindowAnchor(evt.screenPoint));
-    // }
-    // map.on("click", showInfoWindow)
 
-    //Use the ImageParameters to set the visibleLayerIds layers in the map service during ArcGISDynamicMapServiceLayer construction.
+    //image parameters are set to be called in the map service construction
     var imageParameters = new ImageParameters();
     imageParameters.layerIds = [0,1,3,4,5,6,7,8,9,10,11];
     imageParameters.layerOption = ImageParameters.LAYER_OPTION_HIDE;
-    //can also be: LAYER_OPTION_EXCLUDE, LAYER_OPTION_HIDE, LAYER_OPTION_INCLUDE
 
-    layer = new ArcGISDynamicMapServiceLayer("http://gis.uspatial.umn.edu/arcgis/rest/services/CrisisMappingFinal2/MapServer",
+    //map service containing individual risk layers
+    var layer = new ArcGISDynamicMapServiceLayer("http://gis.uspatial.umn.edu/arcgis/rest/services/CrisisMappingFinal2/MapServer",
         {"imageParameters": imageParameters});
     map.addLayer(layer);
 
+    //3 feature layers created for the nuclear sites. 0 and 1 are the buffers. 2 is the points.
     var nuclearLayer0 = new FeatureLayer("http://gis.uspatial.umn.edu/arcgis/rest/services/CrisisMappingFinal2/MapServer/0",{
       visible: false
     });
@@ -104,6 +100,7 @@ require([
     });
     map.addLayer(nuclearLayer2);
 
+    //function for showing all three nuclear feature layers with one button
     on(dom.byId("nuclearpointscheckbox"), "change", function(){
       var box = dom.byId("nuclearpointscheckbox");
       if (box.checked){
@@ -118,167 +115,33 @@ require([
       }
     });
 
-    on(dom.byId("layer4CheckBox"), "change", function(){
-      var box = dom.byId("layer4CheckBox");
-      var visible = [2];
-      if (box.checked){
-        var not_visible = query(".nuclear").siblings();
-        for (var i = 0; i < not_visible.length; i++) {
-          not_visible[i].checked = false;
+    //each hazard button has a function for unchecking other hazards off when it is checked. exception: nuclear sites can be visible on top of any hazard.
+    var layer_list_checkbox_spans = query('.list-item-span');
+
+    for (var i =0; i < layer_list_checkbox_spans.length;i++){
+
+      on(layer_list_checkbox_spans[i].querySelector("input"),"change", function(){
+        //layer 2, state borders, is always visible
+        var visible = [2];
+
+        if (this.checked){
+
+          var not_visible = query(this)
+                              .parent()
+                              .siblings(".list-item-span")
+                              .query(".list_item");
+
+          for (var i = 0; i < not_visible.length; i++) {
+            not_visible[i].checked = false;
+          }
+          visible.push(this.value);
+          layer.setVisibleLayers(visible);
         }
-        visible.push(box.value);
-        layer.setVisibleLayers(visible);
-      }
-      else {
-        visible.push(-1);
-        layer.setVisibleLayers(visible);
-      }
-    }
-  );
 
-    on(dom.byId("layer5CheckBox"), "change", function(){
-      var box = dom.byId("layer5CheckBox");
-      var visible = [2];
-      if (box.checked){
-        var not_visible = query(".earthquakes").siblings();
-        for (var i = 0; i < not_visible.length; i++) {
-          not_visible[i].checked = false;
+        else {
+          visible.push(-1);
+          layer.setVisibleLayers(visible);
         }
-        visible.push(box.value);
-        layer.setVisibleLayers(visible);
-      }
-      else {
-        visible.push(-1);
-        layer.setVisibleLayers(visible);
-      }
+      });
     }
-  );
-
-  on(dom.byId("layer6CheckBox"), "change", function(){
-    var box = dom.byId("layer6CheckBox");
-    var visible = [2];
-    if (box.checked){
-      var not_visible = query(".hurricanes").siblings();
-      for (var i = 0; i < not_visible.length; i++) {
-        not_visible[i].checked = false;
-      }
-      visible.push(box.value);
-      layer.setVisibleLayers(visible);
-    }
-    else {
-      visible.push(-1);
-      layer.setVisibleLayers(visible);
-    }
-  }
-);
-
-on(dom.byId("layer7CheckBox"), "change", function(){
-  var box = dom.byId("layer7CheckBox");
-  var visible = [2];
-  if (box.checked){
-    var not_visible = query(".volcanoes").siblings();
-    for (var i = 0; i < not_visible.length; i++) {
-      not_visible[i].checked = false;
-    }
-    visible.push(box.value);
-    layer.setVisibleLayers(visible);
-  }
-  else {
-    visible.push(-1);
-    layer.setVisibleLayers(visible);
-  }
-}
-);
-
-on(dom.byId("layer8CheckBox"), "change", function(){
-  var box = dom.byId("layer8CheckBox");
-  var visible = [2];
-  if (box.checked){
-    var not_visible = query(".floods").siblings();
-    for (var i = 0; i < not_visible.length; i++) {
-      not_visible[i].checked = false;
-    }
-    visible.push(box.value);
-    layer.setVisibleLayers(visible);
-  }
-  else {
-    visible.push(-1);
-    layer.setVisibleLayers(visible);
-  }
-}
-);
-
-on(dom.byId("layer9CheckBox"), "change", function(){
-  var box = dom.byId("layer9CheckBox");
-  var visible = [2];
-  if (box.checked){
-    var not_visible = query(".winter").siblings();
-    for (var i = 0; i < not_visible.length; i++) {
-      not_visible[i].checked = false;
-    }
-    visible.push(box.value);
-    layer.setVisibleLayers(visible);
-  }
-  else {
-    visible.push(-1);
-    layer.setVisibleLayers(visible);
-  }
-}
-);
-
-on(dom.byId("layer10CheckBox"), "change", function(){
-  var box = dom.byId("layer10CheckBox");
-  var visible = [2];
-  if (box.checked){
-    var not_visible = query(".wildfires").siblings();
-    for (var i = 0; i < not_visible.length; i++) {
-      not_visible[i].checked = false;
-    }
-    visible.push(box.value);
-    layer.setVisibleLayers(visible);
-  }
-  else {
-    visible.push(-1);
-    layer.setVisibleLayers(visible);
-  }
-}
-);
-
-on(dom.byId("layer11CheckBox"), "change", function(){
-  var box = dom.byId("layer11CheckBox");
-  var visible = [2];
-  if (box.checked){
-    var not_visible = query(".tornadoes").siblings();
-    for (var i = 0; i < not_visible.length; i++) {
-      not_visible[i].checked = false;
-    }
-    visible.push(box.value);
-    layer.setVisibleLayers(visible);
-  }
-  else {
-    visible.push(-1);
-    layer.setVisibleLayers(visible);
-  }
-}
-);
-    //on(dom.byId("layer0CheckBox"), "change", updateLayerVisibility);
-    //on(dom.byId("layer1CheckBox"), "change", updateLayerVisibility);
-
-    // function updateLayerVisibility () {
-    //   var inputs = query(".list_item");
-    //   var inputCount = inputs.length;
-    //   visibleLayerIds = [];
-    //
-    //   for (var i = 0; i < inputCount; i++) {
-    //     if (inputs[i].checked) {
-    //       visibleLayerIds.push(inputs[i].value);
-    //     }
-    //   }
-    //
-    //   if (visibleLayerIds.length === 0) {
-    //     visibleLayerIds.push(-1);
-    //   }
-    //
-    //   layer.setVisibleLayers(visibleLayerIds);
-    // }
-  });
+});
